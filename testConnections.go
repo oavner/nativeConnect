a7c0db868343dff341a7f88d2112eda04be82ff5
oavner <oavner@trigoretail.com>
2022-09-20 12:04:14 +0300

    changed the test to create connections in seperate go routines w/ wait groups of 2

--- testConnections.go
+++ testConnections.go
@@ -4,14 +4,10 @@ import (
   "fmt"
   "net/http"
   "io/ioutil"
+  "sync"
 )
 
-func main() {
-
-  url := "http://10.155.0.113:30606/"
-  method := "GET"
-  connectionLimit := 10000
-
+func sendHttpReq(url string, method string, wg *sync.WaitGroup) {
   client := &http.Client {
   }
   req, err := http.NewRequest(method, url, nil)
@@ -21,19 +17,33 @@ func main() {
     return
   }
 
-  for connection := 0; connection < connectionLimit; connection++ {
-      res, err := client.Do(req)
-      if err != nil {
-        fmt.Println(err)
-        return
-      }
-      defer res.Body.Close()
-
-      body, err := ioutil.ReadAll(res.Body)
-      if err != nil {
-        fmt.Println(err)
-        return
-      }
-      fmt.Println(string(body))
-    }
+  res, err := client.Do(req)
+  if err != nil {
+    fmt.Println(err)
+    return
   }
+  defer res.Body.Close()
+
+  body, err := ioutil.ReadAll(res.Body)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println(string(body))
+
+  wg.Done()
+}
+
+func main() {
+  var wg sync.WaitGroup
+  wg.Add(2)
+
+  url := "http://10.155.0.113:30606/"
+  method := "GET"
+  maxConnections := 1000
+
+  for conn := 0; conn < maxConnections; conn++ {
+    go sendHttpReq(url, method, &wg)    
+  }
+}
+  
